c4c8d1b8753612fb14300088507cc1a2379a7a7e
tlothering <32324404+tlothering@users.noreply.github.com>
2020-06-08 08:43:20 +0200

    Update Azure_Create_1_ACvSBC - Az.ps1

--- Azure_Create_1_ACvSBC - Az.ps1
+++ Azure_Create_1_ACvSBC - Az.ps1
@@ -1,4 +1,4 @@
-﻿# Welcome
+# Welcome
     Clear-Host
     Write-Host "This PowerShell script will deploy an AudioCodes VE Session Border Controller in Azure."
     Write-Host "The provisioning process will take up to 10 minutes."
@@ -6,21 +6,21 @@
     Write-Host ""
 
 # Allow Remote Signed Policies
-	Write-Host "Setting Azure PowerShell variables..." -foreground "green"
-	Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy RemoteSigned -Force
+    Write-Host "Setting Azure PowerShell variables..." -foreground "green"
+    Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy RemoteSigned -Force
     Import-Module Az
 #   Set-Item Env:\SuppressAzurePowerShellBreakingChangeWarnings "true"
 
 # Login & Select Azure Subscription
-	Write-Host "Logging in..." -foreground "green";
+    Write-Host "Logging in..." -foreground "green";
     Connect-AzAccount
-	$Subscriptions = Get-AzSubscription
+    $Subscriptions = Get-AzSubscription
     Write-Host ""
 	Write-Host "Please select the Azure Subscription you want to deploy the AudioCodes VE SBC to: " -foreground "green"
 		For ($i=0; $i -lt $Subscriptions.Count; $i++)  {
 			Write-Host "$($i+1): $($Subscriptions[$i].Name)"
 		}	
-	[int]$number = Read-Host "Select your Azure Subscription: "
+    [int]$number = Read-Host "Select your Azure Subscription: "
     Write-Host ""	
     Write-Host "You've selected $($Subscriptions[$number-1].Name)." -foreground "green"
 
@@ -36,13 +36,13 @@
     $VNetAddressSpace = "192.192.0.0/22"
     $SubnetAddressPrefix1 = "192.192.0.0/24"
     $SubnetAddressPrefix2 = "192.192.1.0/24"
-	$NSG1 = $ResourceName + "-TEAMS-NSG"
-	$NSG2 = $ResourceName + "-PSTN-NSG"
+    $NSG1 = $ResourceName + "-TEAMS-NSG"
+    $NSG2 = $ResourceName + "-PSTN-NSG"
     $VMName1 = $ResourceName + "-ACvSBC-01"
-	$Interface11Name = $VMName1 + "-INF-TEAMS"
-	$Interface12Name = $VMName1 + "-INF-PSTN"
-	$PublicIPName11 = $VMName1 + "-PIP-TEAMS"
-	$PublicIPName12 = $VMName1 + "-PIP-PSTN"
+    $Interface11Name = $VMName1 + "-INF-TEAMS"
+    $Interface12Name = $VMName1 + "-INF-PSTN"
+    $PublicIPName11 = $VMName1 + "-PIP-TEAMS"
+    $PublicIPName12 = $VMName1 + "-PIP-PSTN"
     $Location = "SouthAfricaNorth"
     $VMSize = "Standard_B2s"
     $AdminUsername = "acadmin"
@@ -60,15 +60,15 @@
     Write-Host ""
     Write-Host "Microsoft Teams Subnet Name                 : " $Subnet1Name
     Write-Host "Microsoft Teams Subnet Address              : " $SubnetAddressPrefix1
-	Write-Host "Microsoft Teams Interface Name              : " $Interface11Name
+    Write-Host "Microsoft Teams Interface Name              : " $Interface11Name
     Write-Host "Microsoft Teams Network Security Group Name : " $NSG1
-	Write-Host "Microsoft Teams Public IP Name              : " $PublicIPName11
+    Write-Host "Microsoft Teams Public IP Name              : " $PublicIPName11
     Write-Host ""
     Write-Host "PSTN/SIP Trunk Subnet Name                  : " $Subnet2Name
     Write-Host "PSTN/SIP Trunk Subnet Address               : " $SubnetAddressPrefix2
     Write-Host "PSTN/SIP Trunk Interface Name               : " $Interface12Name
-	Write-Host "PSTN/SIP Trunk Network Security Group Name  : " $NSG2
-	Write-Host "PSTN/SIP Trunk Public IP Name               : " $PublicIPName12	
+    Write-Host "PSTN/SIP Trunk Network Security Group Name  : " $NSG2
+    Write-Host "PSTN/SIP Trunk Public IP Name               : " $PublicIPName12	
     Write-Host ""
     Write-Host "Virtual Machine Name                        : " $VMName1
     Write-Host "Virtual Machine Location                    : " $Location
@@ -98,14 +98,14 @@ Start-Sleep -s 5
 Start-Sleep -s 5
 
 # Create Network Security Groups for Teams & PSTN Interfaces
-	$Teamsrule1 = New-AzNetworkSecurityRuleConfig -Name O365_SIP_Signalling -Description "O365 SIP Signalling Port" -Access Allow -Protocol * -Direction Inbound -Priority 100 -SourceAddressPrefix 52.114.148.0/32,52.114.132.46/32,52.114.75.24/32,52.114.76.76/32,52.114.7.24/32,52.114.14.70/32 -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 5061
-	$TeamsRule2 = New-AzNetworkSecurityRuleConfig -Name Media_Ports -Description "Media Ports" -Access Allow -Protocol Udp -Direction Inbound -Priority 101 -SourceAddressPrefix 52.112.0.0/14 -SourcePortRange 10000-10999,49152-53247 -DestinationAddressPrefix * -DestinationPortRange 10000-10999
-	$TeamsRule3 = New-AzNetworkSecurityRuleConfig -Name HTTP_Management -Description "HTTP Management of AudioCodes VE SBC" -Access Allow -Protocol Tcp -Direction Inbound -Priority 200 -SourceAddressPrefix Internet -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 80,443
-	$NSGTeams = New-AzNetworkSecurityGroup -Name $NSG1 -ResourceGroupName $ResourceGroupName -Location $Location -SecurityRules $TeamsRule1,$TeamsRule2,$TeamsRule3
+    $Teamsrule1 = New-AzNetworkSecurityRuleConfig -Name O365_SIP_Signalling -Description "O365 SIP Signalling Port" -Access Allow -Protocol * -Direction Inbound -Priority 100 -SourceAddressPrefix 52.114.148.0/32,52.114.132.46/32,52.114.75.24/32,52.114.76.76/32,52.114.7.24/32,52.114.14.70/32 -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 5061
+    $TeamsRule2 = New-AzNetworkSecurityRuleConfig -Name Media_Ports -Description "Media Ports" -Access Allow -Protocol Udp -Direction Inbound -Priority 101 -SourceAddressPrefix 52.112.0.0/14 -SourcePortRange 10000-10999,49152-53247 -DestinationAddressPrefix * -DestinationPortRange 10000-10999
+    $TeamsRule3 = New-AzNetworkSecurityRuleConfig -Name HTTP_Management -Description "HTTP Management of AudioCodes VE SBC" -Access Allow -Protocol Tcp -Direction Inbound -Priority 200 -SourceAddressPrefix Internet -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 80,443
+    $NSGTeams = New-AzNetworkSecurityGroup -Name $NSG1 -ResourceGroupName $ResourceGroupName -Location $Location -SecurityRules $TeamsRule1,$TeamsRule2,$TeamsRule3
 	
-	$PSTNRule1 = New-AzNetworkSecurityRuleConfig -Name SIP_Trunk -Description "SIP Trunk Port" -Access Allow -Protocol Tcp -Direction Inbound -Priority 100 -SourceAddressPrefix Internet -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 5060
-	$PSTNRule2 = New-AzNetworkSecurityRuleConfig -Name HTTP_Management -Description "HTTP Management of AudioCodes VE SBC" -Access Allow -Protocol Tcp -Direction Inbound -Priority 200 -SourceAddressPrefix Internet -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 80,443
-	$NSGPSTN = New-AzNetworkSecurityGroup -Name $NSG2 -ResourceGroupName $ResourceGroupName -Location $Location -SecurityRules $PSTNRule1,$PSTNRule2
+    $PSTNRule1 = New-AzNetworkSecurityRuleConfig -Name SIP_Trunk -Description "SIP Trunk Port" -Access Allow -Protocol Tcp -Direction Inbound -Priority 100 -SourceAddressPrefix Internet -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 5060
+    $PSTNRule2 = New-AzNetworkSecurityRuleConfig -Name HTTP_Management -Description "HTTP Management of AudioCodes VE SBC" -Access Allow -Protocol Tcp -Direction Inbound -Priority 200 -SourceAddressPrefix Internet -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 80,443
+    $NSGPSTN = New-AzNetworkSecurityGroup -Name $NSG2 -ResourceGroupName $ResourceGroupName -Location $Location -SecurityRules $PSTNRule1,$PSTNRule2
 
 Start-Sleep -s 5
 
@@ -141,8 +141,8 @@ Start-Sleep -s 5
 # Define the AudioCodes Source Image
     Write-Host "Setting Azure AudioCodes SBC Image as Source..." -foreground "green";
     Get-AzMarketplaceTerms -Publisher "audiocodes" -Product "mediantsessionbordercontroller" -Name "mediantvirtualsbcazure" | Set-AzMarketplaceTerms -Accept
-	Set-AzVMSourceImage -VM $VM1 -PublisherName audiocodes -Offer mediantsessionbordercontroller -Skus mediantvirtualsbcazure -Version latest
-	Set-AzVMPlan -VM $VM1 -Name mediantvirtualsbcazure -Publisher audiocodes -Product mediantsessionbordercontroller
+    Set-AzVMSourceImage -VM $VM1 -PublisherName audiocodes -Offer mediantsessionbordercontroller -Skus mediantvirtualsbcazure -Version latest
+    Set-AzVMPlan -VM $VM1 -Name mediantvirtualsbcazure -Publisher audiocodes -Product mediantsessionbordercontroller
 
 Start-Sleep -s 5
 
